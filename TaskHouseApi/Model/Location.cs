342cb326762e3b93c212d1700e79bdc81972df37
alex6296 <32260519+alex6296@users.noreply.github.com>
2018-10-16 16:22:42 +0200

    added models
    - finished user model
    -added location model

---
+++ TaskHouseApi/Model/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TaskHouseApi.Model
+{
+
+    public class Location
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+        public string zipCode { get; set; }
+        public string primaryLine { get; set; }
+        public string secondaryLine { get; set; }
+
+       
+
+        }
+    }
+}
