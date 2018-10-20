1e288277dd694cd5d551997af7e4b6d662c4fa4c
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-10-20 19:49:13 +0200

    Salted and hashed password v1

--- TaskHouseApi/Model/User.cs
+++ TaskHouseApi/Model/User.cs
@@ -11,6 +11,7 @@ namespace TaskHouseApi.Model
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Salt { get; set; }
 
         public User()
         {
