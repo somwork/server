e797402576564d94fa41b01d891bacf3c1cbd6d6
GGkarma <jepper01@hotmail.com>
2018-10-23 11:58:01 +0200

    Worker-controller

---
+++ TaskHouseApi/Model/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskHouseApi.Model
+{
+
+    public class Worker : User
+    {
+        public List<Education> Educations { get; set; }
+        
+        public Worker()
+        {
+        }
+    }
+}
