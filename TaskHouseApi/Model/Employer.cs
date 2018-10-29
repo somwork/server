3d5a8970dc2e8a68d6e069d788804a17cb6f1cf2
GGkarma <32382976+GGkarma@users.noreply.github.com>
2018-10-29 13:05:19 +0100

    Started on employer controller

---
+++ TaskHouseApi/Model/Employer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskHouseApi.Model
+{
+
+    public class Employer : User
+    {
+       
+        public Employer()
+        {
+        }
+    }
+}
