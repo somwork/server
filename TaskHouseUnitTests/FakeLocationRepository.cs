6e8d5229d2b7e31c14d90dd6605e75c7fb9a0e52
NJohansen <32328015+NJohansen@users.noreply.github.com>
2018-11-02 11:12:44 +0100

    Indent fixed

--- TaskHouseUnitTests/FakeLocationRepository.cs
+++ TaskHouseUnitTests/FakeLocationRepository.cs
@@ -9,81 +9,81 @@ using System.Threading.Tasks;
 
 namespace TaskHouseUnitTests
 {
-  public class FakeLocationRepository : ILocationRepository
-  {
-    private static List<Location> locationCache;
-
-    public FakeLocationRepository()
+    public class FakeLocationRepository : ILocationRepository
     {
-      locationCache = new List<Location>()
-      {
-        new Location()
-        {
-          Id = 1,
-          Country = "Country1",
-          City = "City1",
-          ZipCode = "ZipCode1" ,
-          PrimaryLine = "PrimaryLine1" ,
-          SecondaryLine = "SecondaryLine1" ,
-          },
-          new Location()
-          {
-            Id = 2,
-            Country = "Country2",
-            City = "City2",
-            ZipCode = "ZipCode2" ,
-            PrimaryLine = "PrimaryLine2" ,
-            SecondaryLine = "SecondaryLine2" ,
-            },
-            new Location()
-            {
-              Id = 3,
-              Country = "Country3",
-              City = "City3",
-              ZipCode = "ZipCode3" ,
-              PrimaryLine = "PrimaryLine3" ,
-              SecondaryLine = "SecondaryLine3" ,
-              },
-            };
-          }
+        private static List<Location> locationCache;
 
-          public async Task<Location> Create(Location l)
-          {
+        public FakeLocationRepository()
+        {
+            locationCache = new List<Location>()
+            {
+                new Location()
+                {
+                    Id = 1,
+                    Country = "Country1",
+                    City = "City1",
+                    ZipCode = "ZipCode1",
+                    PrimaryLine = "PrimaryLine1",
+                    SecondaryLine = "SecondaryLine1",
+                },
+                new Location()
+                {
+                    Id = 2,
+                    Country = "Country2",
+                    City = "City2",
+                    ZipCode = "ZipCode2",
+                    PrimaryLine = "PrimaryLine2",
+                    SecondaryLine = "SecondaryLine2",
+                },
+                new Location()
+                {
+                    Id = 3,
+                    Country = "Country3",
+                    City = "City3",
+                    ZipCode = "ZipCode3",
+                    PrimaryLine = "PrimaryLine3",
+                    SecondaryLine = "SecondaryLine3",
+                },
+            };
+        }
+
+        public async Task<Location> Create(Location l)
+        {
             locationCache.Add(l);
             return l;
-          }
+        }
 
-          public async Task<Location> Retrieve(int Id)
-          {
+        public async Task<Location> Retrieve(int Id)
+        {
             return locationCache.Where(l => l.Id == Id).SingleOrDefault();
-          }
+        }
 
-          public async Task<IEnumerable<Location>> RetrieveAll()
-          {
+        public async Task<IEnumerable<Location> > RetrieveAll()
+        {
             return locationCache;
-          }
+        }
 
-          public async Task<Location> Update(int Id, Location location)
-          {
+        public async Task<Location> Update(int Id, Location location)
+        {
             Location old = locationCache.Where(l => l.Id == Id).SingleOrDefault();
             int index = locationCache.IndexOf(old);
 
             locationCache[index] = location;
             return location;
-          }
+        }
 
-          public async Task<bool> Delete(int Id)
-          {
+        public async Task<bool> Delete(int Id)
+        {
             Location location = locationCache.Where(l => l.Id == Id).SingleOrDefault();
 
             if(location == null)
             {
-              return false;
+                return false;
             }
 
             locationCache.Remove(location);
             return true;
-          }
-
         }
-      }
+
+    }
+}
--- TaskHouseUnitTests/LocationsControllerUnitTests.cs
+++ TaskHouseUnitTests/LocationsControllerUnitTests.cs
@@ -10,179 +10,179 @@ using System.Linq;
 
 namespace TaskHouseUnitTests
 {
-  public class LocationsControllerUnitTests
-  {
-    ILocationRepository repo;
-    LocationsController controller;
-
-    public LocationsControllerUnitTests()
+    public class LocationsControllerUnitTests
     {
-      repo = new FakeLocationRepository();
-      controller = new LocationsController(repo);
+        ILocationRepository repo;
+        LocationsController controller;
+
+        public LocationsControllerUnitTests()
+        {
+            repo = new FakeLocationRepository();
+            controller = new LocationsController(repo);
+        }
+
+        //Test retrieve all in repository
+        [Fact]
+        public async void LocationsController_Get_ReturnAllElementsInRepo()
+        {
+            //Arrange object
+            IEnumerable<Location> resultset;
+
+            //Act
+            resultset = await controller.Get();
+
+            //Assert - Checks the size of the list matches with 3 which is the size
+            Assert.Equal(3,resultset.Count());
+        }
+
+        [Fact]
+        //Test GET with id
+        public async void LocationsController_Get_ReturnObject_WhenIdIsValid()
+        {
+            //Arrange id for location object
+            int locationId = 1;
+
+            //Act
+            var result = await controller.Get(locationId);
+            var resultAsObject = await controller.Get(locationId) as ObjectResult;
+            var resultObject = resultAsObject.Value as Location;
+
+            //Assert - Checks if the returned object is the same type and then checks id
+            Assert.IsType<ObjectResult>(result);
+            Assert.Equal(locationId, resultObject.Id);
+        }
+
+        [Fact]
+        // Test GET with invalid ID
+        public  async void LocationController_Get_ReturnsNotFound_WhenGivenInvalidId()
+        {
+            //Arrange id for location object
+            int locationId = 403;
+
+            //Act
+            var result = await controller.Get(locationId) as NotFoundResult;
+
+            //Assert
+            Assert.Equal(404, result.StatusCode);
+        }
+
+        [Fact]
+        //Test POST for creating new location
+        public async void LocationController_Create_ReturnsObject_WhenNewObject()
+        {
+            //Arrange new ObjectResult
+            var location = new Location();
+            location.Country = "Contry100";
+
+            //Act
+            var result = await controller.Create(location);
+            var resultAsObject = await controller.Create(location) as ObjectResult;
+            var resultObject = resultAsObject.Value as Location;
+
+            //Assert
+            Assert.IsType<ObjectResult>(result);
+            Assert.Equal(location.Country,resultObject.Country);
+        }
+
+        [Fact]
+        //Test POST for creating new location that is null
+        public async void LocationController_Create_ReturnsBadRequest_WhenObjectIsNull()
+        {
+            //Arrange new ObjectResult
+            Location location = null;
+
+            //Act
+            var result = await controller.Create(location);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        // Test PUT for update Location
+        public async void LocationController_Update_ReturnsNoContentResultAndCreatedbject_WhenParametersAreValid()
+        {
+            //Arrange
+            Location location = new Location()
+            {
+                Id = 1,
+                Country = "UpdatedCountry"
+            };
+
+            //Act
+            var result = await controller.Update(location.Id,location);
+            var resultAsObject = await controller.Get(location.Id) as ObjectResult;
+            var resultObject = resultAsObject.Value as Location;
+
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal(location.Country, resultObject.Country);
+        }
+
+        ///Test put with invalid Id and null Location object
+        [Fact]
+        public async void LocationController_Update_ReturnsBadRequestResult_WhenIdIsInvalidAndLocationIsNull()
+        {
+            //Arrange
+            Location location = null;
+            int Id = 100;
+
+            //Act
+            var result = await controller.Update(Id, location);
+
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+
+        }
+
+        [Fact]
+        // Test PUT for update Location when Id is invalid
+        public async void LocationController_Update_ReturnsBadRequest_WhenIdIsInvalid()
+        {
+            //Arrange
+            Location location = new Location()
+            {
+                Id = 100,
+                Country = "UpdatedCountry"
+            };
+
+            //Act
+            var result = await controller.Update(location.Id,location);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        // Test DELETE for Location
+        public async void LocationController_Delete_ReturnsNoContentResult_WhenDeleted()
+        {
+            //Arrange
+            int Id = 1;
+
+            //Act
+            var result = await controller.Delete(Id);
+            var getDeletedResult = await controller.Get(Id);
+
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+            Assert.IsType<NotFoundResult>(getDeletedResult);
+        }
+
+        [Fact]
+        //Test DELETE for invalid Id for Location
+        public async void LocationController_Delete_ReturnsNotFoundResult_WhenIdInvalid()
+        {
+            //Arrange
+            int Id = 100;
+
+            //Act
+            var result = await controller.Delete(Id);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+
+        }
+
     }
-
-    //Test retrieve all in repository
-    [Fact]
-    public async void LocationsController_Get_ReturnAllElementsInRepo()
-    {
-      //Arrange object
-      IEnumerable<Location> resultset;
-
-      //Act
-      resultset = await controller.Get();
-
-      //Assert - Checks the size of the list matches with 3 which is the size
-      Assert.Equal(3,resultset.Count());
-    }
-
-    [Fact]
-    //Test GET with id
-    public async void LocationsController_Get_ReturnObject_WhenIdIsValid()
-    {
-      //Arrange id for location object
-      int locationId = 1;
-
-      //Act
-      var result = await controller.Get(locationId);
-      var resultAsObject = await controller.Get(locationId) as ObjectResult;
-      var resultObject = resultAsObject.Value as Location;
-
-      //Assert - Checks if the returned object is the same type and then checks id
-      Assert.IsType<ObjectResult>(result);
-      Assert.Equal(locationId, resultObject.Id);
-    }
-
-    [Fact]
-    // Test GET with invalid ID
-    public  async void LocationController_Get_ReturnsNotFound_WhenGivenInvalidId()
-    {
-      //Arrange id for location object
-      int locationId = 403;
-
-      //Act
-      var result = await controller.Get(locationId) as NotFoundResult;
-
-      //Assert
-      Assert.Equal(404, result.StatusCode);
-    }
-
-    [Fact]
-    //Test POST for creating new location
-    public async void LocationController_Create_ReturnsObject_WhenNewObject()
-    {
-      //Arrange new ObjectResult
-      var location = new Location();
-      location.Country = "Contry100";
-
-      //Act
-      var result = await controller.Create(location);
-      var resultAsObject = await controller.Create(location) as ObjectResult;
-      var resultObject = resultAsObject.Value as Location;
-
-      //Assert
-      Assert.IsType<ObjectResult>(result);
-      Assert.Equal(location.Country,resultObject.Country);
-    }
-
-    [Fact]
-    //Test POST for creating new location that is null
-    public async void LocationController_Create_ReturnsBadRequest_WhenObjectIsNull()
-    {
-      //Arrange new ObjectResult
-      Location location = null;
-
-      //Act
-      var result = await controller.Create(location);
-
-      //Assert
-      Assert.IsType<BadRequestObjectResult>(result);
-    }
-
-    [Fact]
-    // Test PUT for update Location
-    public async void LocationController_Update_ReturnsNoContentResultAndCreatedbject_WhenParametersAreValid()
-    {
-      //Arrange
-      Location location = new Location()
-      {
-        Id = 1,
-        Country = "UpdatedCountry"
-      };
-
-      //Act
-      var result = await controller.Update(location.Id,location);
-      var resultAsObject = await controller.Get(location.Id) as ObjectResult;
-      var resultObject = resultAsObject.Value as Location;
-
-      //Assert
-      Assert.IsType<NoContentResult>(result);
-      Assert.Equal(location.Country, resultObject.Country);
-    }
-
-    ///Test put with invalid Id and null Location object
-    [Fact]
-    public async void LocationController_Update_ReturnsBadRequestResult_WhenIdIsInvalidAndLocationIsNull()
-    {
-      //Arrange
-      Location location = null;
-      int Id = 100;
-
-      //Act
-      var result = await controller.Update(Id, location);
-
-      //Assert
-      Assert.IsType<BadRequestResult>(result);
-
-    }
-
-    [Fact]
-    // Test PUT for update Location when Id is invalid
-    public async void LocationController_Update_ReturnsBadRequest_WhenIdIsInvalid()
-    {
-      //Arrange
-      Location location = new Location()
-      {
-        Id = 100,
-        Country = "UpdatedCountry"
-      };
-
-      //Act
-      var result = await controller.Update(location.Id,location);
-
-      //Assert
-      Assert.IsType<NotFoundResult>(result);
-    }
-
-    [Fact]
-    // Test DELETE for Location
-    public async void LocationController_Delete_ReturnsNoContentResult_WhenDeleted()
-    {
-      //Arrange
-      int Id = 1;
-
-      //Act
-      var result = await controller.Delete(Id);
-      var getDeletedResult = await controller.Get(Id);
-
-      //Assert
-      Assert.IsType<NoContentResult>(result);
-      Assert.IsType<NotFoundResult>(getDeletedResult);
-    }
-
-    [Fact]
-    //Test DELETE for invalid Id for Location
-    public async void LocationController_Delete_ReturnsNotFoundResult_WhenIdInvalid()
-    {
-      //Arrange
-      int Id = 100;
-
-      //Act
-      var result = await controller.Delete(Id);
-
-      //Assert
-      Assert.IsType<NotFoundResult>(result);
-
-    }
-
-  }
 }
