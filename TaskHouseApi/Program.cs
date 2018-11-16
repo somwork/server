400a1452636cca8baf5d5e3b1151ae75b28da42a
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-11-16 20:17:21 +0100

    Seed not working so removede

--- TaskHouseApi/Program.cs
+++ TaskHouseApi/Program.cs
@@ -18,27 +18,7 @@ namespace TaskHouseApi
     {
         public static void Main(string[] args)
         {
-            //CreateWebHostBuilder(args).Build().Run();
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<PostgresContext>();
-                    context.Database.Migrate();
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
