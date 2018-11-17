9d9b59320c71e2ee27e18fc03525b028fe18e6ee
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-11-17 22:55:21 +0100

    Fix test

--- TaskHouseUnitTests/FakeRepository.cs
+++ TaskHouseUnitTests/FakeRepository.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using TaskHouseApi.Model;
 using TaskHouseApi.Persistence.Repositories.Interfaces;
 using System.Linq;
+using System.Reflection;
+using System.Collections;
 
 namespace TaskHouseUnitTests
 {
@@ -50,7 +52,49 @@ namespace TaskHouseUnitTests
 
         public void UpdatePart(T baseModel, string[] nameOfPropertysToIgnore)
         {
-            throw new System.NotImplementedException();
+            T oldModel = Retrieve(baseModel.Id);
+            int index = list.IndexOf(oldModel);
+
+            PropertyInfo[] propertyInfos = baseModel.GetType().GetProperties();
+            if (propertyInfos.Count() == 0)
+            {
+                return;
+            }
+
+            foreach (PropertyInfo property in propertyInfos)
+            {
+                /// If the as ICollection or name is Id do nothing
+                if (property.GetValue(baseModel) is ICollection || property.Name == "Id")
+                {
+                    continue;
+                }
+
+                /// If property name is in ignore is don't change the value
+                if (nameOfPropertysToIgnore.Contains(property.Name))
+                {
+                    continue;
+                }
+
+                /// Gets the property value
+                var propertyValue = property.GetValue(baseModel);
+
+                /// If the value is null
+                if (propertyValue == null)
+                {
+                    /// ??????????
+                    /// SHOULD THE property be set to null ?
+                    /// ??????????
+                    continue;
+                }
+
+                /// Marks the property as modified
+                oldModel.GetType().GetProperty(property.Name).SetValue(oldModel, propertyValue);
+            }
+
+            if (index != -1)
+            {
+                list[index] = oldModel;
+            }
         }
     }
 }
