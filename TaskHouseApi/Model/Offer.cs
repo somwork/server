9f1d23d940b229290507edfa484316bdec1b3157
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-11-19 15:33:26 +0100

    Update Offer.cs

--- TaskHouseApi/Model/Offer.cs
+++ TaskHouseApi/Model/Offer.cs
@@ -5,7 +5,6 @@ namespace TaskHouseApi.Model
         public bool Accepted { get; set; }
         public decimal Price { get; set; }
         public string Currency { get; set; }
-
         public int WorkerId { get; set; }
         public int TaskId { get; set; }
 
