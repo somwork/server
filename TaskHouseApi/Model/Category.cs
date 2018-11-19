cba783dfac97ba283384607740cc2f6cd429c99a
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-11-19 15:50:51 +0100

    Migrations and database setup (#59)

--- TaskHouseApi/Model/Category.cs
+++ TaskHouseApi/Model/Category.cs
@@ -6,12 +6,13 @@ namespace TaskHouseApi.Model
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<Task> Tasks { get; set; }
+        public virtual ICollection<CategorySkill> CategorySkill { get; set; }
+        public virtual ICollection<CategoryTask> CategoryTask { get; set; }
 
         public Category()
         {
-
+            CategorySkill = new List<CategorySkill>();
+            CategoryTask = new List<CategoryTask>();
         }
     }
 }
